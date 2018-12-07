3c1504e62a79084a2776e3a6245ee6faf6b59852
Evgenii Nikitin <en919@nyu.edu>
2018-12-07 07:23:30 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nlpclass',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Translation Model for NLP class (NYU)',
+    author='Evgenii Nikitin',
+    license='MIT',
+)
